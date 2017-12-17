becd39bd4d3394ef16a833308fd093e88381927e
David Mendelovits <david.mendelovits@gmail.com>
2017-12-17 16:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ function prependKitten(name) {
   return [name,...kittens]
 }
 
-function removeLastKitten(name) {
+function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1)
 }
+
+
