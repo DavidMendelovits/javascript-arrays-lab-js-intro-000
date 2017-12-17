3c542444497b1cc0c1f4f32e094279f232bec6e0
David Mendelovits <david.mendelovits@gmail.com>
2017-12-17 15:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  kittens.shift(-1, name)
+  kittens.push(name)
   return kittens
 }
